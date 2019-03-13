44fafadc8dc5e1bf7ecc8e590ee2e859443a0513
Bruno Cavalcante <brunocavalcantedsilva@gmail.com>
2019-03-12 23:26:42 -0300

    page projeto

--- src/pages/projetos/projetos.ts
+++ src/pages/projetos/projetos.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { MenuController } from 'ionic-angular';
 import { TarefasPage } from '../tarefas/tarefas';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Storage } from '@ionic/storage';
-import { identifierModuleUrl } from '@angular/compiler';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase} from '@angular/fire/database';
+
 
 
 @IonicPage()
@@ -14,61 +15,158 @@ import { identifierModuleUrl } from '@angular/compiler';
 })
 export class ProjetosPage {
 
-  operacao = false ;
-  participante=[{
-    status:true
-  }]; 
-  count = 0;
+  operacao = false;
+  uid;
+  id_projeto;
+  list;
+  
+  participante = [{
+    email: "",
+    funcao: "",
+    desc_f: ""
+  }];
   newProjectForm: FormGroup;
-  
 
-  
+
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public menuCtrl: MenuController,
     public formbuilder: FormBuilder,
-    public storage: Storage ) {
-    
-      this.operacao = false ;
-       //Validação dos campos
-       this.newProjectForm = this.formbuilder.group({
-        descricao:[null,[Validators.required,Validators.minLength(20)]],
-        data_ini:[null],
-        data_fim:[null],
-        faculdade:[null],
-        campus:[null],
-        funcao:[null],
-        email:[null],
-        name:[null,[Validators.required,Validators.minLength(5)]],
-      })
+    public afAuth: AngularFireAuth,
+    public db: AngularFireDatabase,
+    public alertCtrl: AlertController) {
+     
+    this.operacao = false;
+    const user = this.afAuth.auth.currentUser;//pega usuario logado
+    this.uid = user.uid;
+    this.getProjeto();
+    //Validação dos campos
+    this.newProjectForm = this.formbuilder.group({
+      descricao: [null, [Validators.required, Validators.minLength(10)]],
+      data_ini: [null],
+      data_fim: [null],
+      faculdade: [null],
+      name: [null, [Validators.required, Validators.minLength(5)]],
+    })
   }
 
   ionViewDidLoad() {
-    console.log(this.operacao);
+    console.log(this.list);
     this.closeMenu();
+    this.participante.splice(0, 1);
+
   }
   closeMenu() {
     this.menuCtrl.close();
   }
-  goToProjetos(){
+  goToProjetos() {
     this.navCtrl.push(TarefasPage);
   }
-  createProjeto(){
+
+  addParticipante() {
+    this.presentPrompt();
+
+  }
+  removeParticipante() {
+    this.participante.splice(0, 1);
+  }
+  createProjeto() {
     this.operacao = true;
   }
 
-  addParticipante(){
-   
-    this.participante[this.count] = {status:true};
-    this.count = this.count+1
-    console.log(this.participante);
-    
-  }
-  removeParticipante(){
+  addProjeto() {
+    this.operacao = true;
+    // criar projeto
+    this.id_projeto = this.db.database.ref(this.uid).child('projetos').push(this.newProjectForm.value).key
 
-    this.participante[this.count-1] = null;
-    this.count = this.count - 1;
+    if (this.id_projeto != null) {
+      // adiciona os participantes ao projeto
+      this.participante.forEach(data => {
+        this.db.database.ref(this.uid + '/projetos/' + this.id_projeto).child('participante').push({
+          email: data.email,
+          funcao: data.funcao,
+          desc_funcao: data.desc_f
+        })
+      });
+
+      this.presentAlert("Projeto Criado!", "Projeto criado com sucesso");
+      this.operacao = false;
+    }
+
   }
+
+  getProjeto() {
+
+    let listDB = this.db.database.ref(this.uid).child('projetos');
+
+    listDB.on('value', (snapshot)=> { //para on escuta qualquer alteração no banco de dados e grava na variavel snapshot 
+          const items = snapshot.val();
+          if(items){ //verificando se existe items
+            this.list = Object.keys(items).map(i => items[i]);//Função atribui cada objeto retornado do banco na variavel list
+            console.log(this.list);
+          }
+        }); 
+  }
+
+  //Alerta de Inputs
+  presentPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'Participante',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Digite o email do participante'
+        },
+        {
+          name: 'funcao',
+          placeholder: 'Digite a Função',
+          type: 'text'
+        },
+        {
+          name: 'desc_f',
+          placeholder: 'Descreva a Função',
+          type: 'text',
+
+        },
+        {
+          name: 'radio1',
+          label: "teste",
+          type: 'checkbox'
+
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'OK',
+          handler: data => {
+            this.participante.push(data);
+
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  //Função para apresenta alertas
+  public presentAlert(title: string, subtitle: string) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subtitle,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
 }
